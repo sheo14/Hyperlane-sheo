2ab05cc32717bdcec552dd639d5d23ad6b62322b
sheo <137234958+sheo14@users.noreply.github.com>
2024-10-16 16:43:43 +0200

    Update app.ts

--- hyperlane-warp-ui-template-main/src/consts/app.ts
+++ hyperlane-warp-ui-template-main/src/consts/app.ts
@@ -10,7 +10,7 @@ export const MAIN_FONT = Space_Grotesk({
 });
 export const APP_NAME = 'SHEO BRIDGE ';
 export const APP_DESCRIPTION = 'Fast bridge powered by hyperlane, welcome to SHEO bridge';
-export const APP_URL = 'sheo-bridge.vercel.app';
+export const APP_URL = 'hyperlane-sheo.vercel.app';
 export const BRAND_COLOR = Color.primary;
 export const BACKGROUND_COLOR = Color.primary;
 export const BACKGROUND_IMAGE = 'url(/backgrounds/main.svg)';
